ce1b1fddde4a49de3545cbbf62fe1cd5b115bba7
Ayan Khandelwal <khandelwalayan11@gmail.com>
2025-03-15 21:35:51 +0530

    Port changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,13 +5,12 @@ import {server} from './utils/socket.io.js'
 import connectDB from './db/index.js'
 dotenv.config({path: './.env'})
 connectDB()
-.then(()=> {
-    server.listen(process.env.PORT || 8000 , ()=>{
-        console.log(`Server is running on port ${process.env.PORT}`)
-    })
-})
-.catch((error)=>{
-    console.log("MongoDb connection error: " + error);
- 
-})
-
+  .then(() => {
+    const PORT = process.env.PORT || 8000; 
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
