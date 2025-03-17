2d35a3c53d1d32516caaea2bbb816f414b6e9295
Ayan Khandelwal <khandelwalayan11@gmail.com>
2025-03-17 10:03:04 +0530

    still trying

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,31 +2,11 @@ import express from "express";
 import cors from "cors";
 import  cookieParser from 'cookie-parser';
 import {app} from './utils/socket.io.js'
-const allowedOrigins = [
-    "https://prime-finance-hbmv.vercel.app",
-    "https://prime-finance-hbmv-hz92asgbd-ayans-projects-991331f8.vercel.app",
-    "https://prime-finance-hbmv-7rkt8pncd-ayans-projects-991331f8.vercel.app"
-];
-
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin: '*', // Allows all origins
     methods: 'GET,POST,PUT,DELETE',
     credentials: true
 }));
-app.options('*', cors());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://prime-finance-hbmv-hz92asgbd-ayans-projects-991331f8.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-});
 app.use(express.json({ limit: "16kb" }));
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
